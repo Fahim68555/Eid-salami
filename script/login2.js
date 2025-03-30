5b94e5052d0776318332c86bbab54cd6c1e217f7
Fahim68555 <programhero541@gmail.com>
2025-03-30 11:33:25 +0600

    add all changes

--- script/login2.js
+++ script/login2.js
@@ -3,16 +3,26 @@ document.getElementById('login-btn').addEventListener('click',function(event){
    const number = document.getElementById('acount-number').value;
    const pin = document.getElementById('pin').value;
    const convertedPin = parseInt(pin);
-   if( number.length === 11 ){
-if(convertedPin === 1234){
+   if( number.length === 11  ){
+const ANumber = [...number];
+if(ANumber[0]==="0" && ANumber[1]==="1"){
+  if(convertedPin === 1234){
     window.location.href='./main.html'
+  }
+  else{
+    alert('Invalid Pin')
+  }
 }
 else{
-    alert('Invalid Pin')
+  alert("Please start with 01")
 }
+
 
   }
   else{
-    alert('Invalid Mobile Number')
+    alert('Invalid Mobile Number (Please give 11 digit account number)')
   }
 })
+
+
+
--- script/recape.js
+++ script/recape.js
@@ -1,55 +1,86 @@
-document.getElementById('add-money').addEventListener('click',function(event){
+document.getElementById('add-money').addEventListener('click', function (event) {
     event.preventDefault();
     const pin = document.getElementById('pin').value;
     const convertedPin = parseInt(pin);
-   const amount = document.getElementById('amount').value;
-   const convertedAmount = parseFloat(amount);
-   const addAmount = document.getElementById('men-amount').innerText;
-   const convertedAddAmount = parseInt(addAmount);
-   if(convertedPin === 1234){
-    const sum = convertedAddAmount + convertedAmount;
-    document.getElementById('men-amount').innerText=sum;
-   }
-   else{
-    console.log('Invalid Pin ')
-   }
+    const amount = document.getElementById('amount').value;
+    const convertedAmount = parseFloat(amount);
+    const addAmount = document.getElementById('men-amount').innerText;
+    const accountNumber =document.getElementById("account-number").value;
+    const convertedAddAmount = parseInt(addAmount);
+    if (convertedPin === 1234) {
+        
+        if(accountNumber.length === 11){
+            const AAccountNumber = [...accountNumber]
+            console.log(AAccountNumber)
+            if(AAccountNumber[0] === '0' && AAccountNumber[1] === '1' ){
+                const sum = convertedAddAmount + convertedAmount;
+                document.getElementById('men-amount').innerText = sum;
+            }
+            else{
+                alert("Invalid AccountNumber (Please start with 01)")
+            }
+           
+        }
+       
+        else{
+            alert("Please give 11 digit account number")
+        }
+       
+    }
+    else {
+        alert('Invalid Pin ')
+    }
 })
-document.getElementById('section-2').style.display='none';
-document.getElementById('section-1').style.display='block';
+document.getElementById('section-2').style.display = 'none';
+document.getElementById('section-1').style.display = 'block';
 
 
-document.getElementById('btn-1').addEventListener('click',function(){
-    document.getElementById('section-2').style.display='none';
-    document.getElementById('section-1').style.display='block';
+document.getElementById('btn-1').addEventListener('click', function () {
+    document.getElementById('section-2').style.display = 'none';
+    document.getElementById('section-1').style.display = 'block';
 })
-document.getElementById('btn-2').addEventListener('click',function(){
-    document.getElementById('section-1').style.display='none';
-    document.getElementById('section-2').style.display='block';
+document.getElementById('btn-2').addEventListener('click', function () {
+    document.getElementById('section-1').style.display = 'none';
+    document.getElementById('section-2').style.display = 'block';
 })
 
-document.getElementById('Cashout').addEventListener('click',function(event){
+document.getElementById('Cashout').addEventListener('click', function (event) {
     event.preventDefault();
     const pin = document.getElementById('pin').value;
     const convertedPin = parseInt(pin);
-   const amount2 = document.getElementById('amount2').value;
-   const convertedAmount = parseFloat(amount2);
-   const addAmount = document.getElementById('men-amount').innerText;
-   const accountNumber=document.getElementById("acount-number").value
-   const convertedAddAmount = parseInt(addAmount);
-   if(convertedPin === 1234){
-    if(accountNumber.length === 11 && accountNumber[0]===0 && accountNumber[1]===1){
-        const sum = convertedAddAmount - convertedAmount;
-        document.getElementById('men-amount').innerText=sum;
+    const amount2 = document.getElementById('amount2').value;
+    const convertedAmount = parseFloat(amount2);
+    const addAmount = document.getElementById('men-amount').innerText;
+    const accountNumber2 =document.getElementById("account-number2").value;
+    const convertedAddAmount = parseInt(addAmount);
+
+
+
+    if (convertedPin === 1234) {
+    if(accountNumber2.length === 11){
+            const AAccountNumber2 = [...accountNumber2]
+            console.log(AAccountNumber2)
+            if(AAccountNumber2[0] === '0' && AAccountNumber2[1] === '1' ){
+                const sum3 = convertedAddAmount - convertedAmount;
+                    document.getElementById('men-amount').innerText = sum3
+                
+                }
+            else{
+                alert("Invalid AccountNumber (Please start with 01)")
+            }
+          
+           
+        }
+        else{
+            alert("Please give 11 digit account number")
+        }
+}
+
+    else {
+        alert('Invalid Pin ')
     }
-    else{
-        alert("Invalid Account Number")
-    }
-    
-    
-    
-   }
-   else{
-    alert('Invalid Pin ')
-   }
+
+
+
 })
-   
+
